26504c8f5b9cc7d6aecba4c54fb063f63ddf48b5
Rajat Som <rajat.som.ug21@nsut.ac.in>
2023-07-11 20:43:46 +0530

    for other branch

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <div className="p-2 bg-primary">
+                <p className="text-center text-white">© 2023 BlogFreakk.com</p>
+
+            </div>
+
+
+
+
+        </>
+    )
+}
+
+export default Footer
